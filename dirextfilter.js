cdb7a2a96f9bf2572ee6743e02589facffec04dc
Wei-Ting Lu <weitingtlu@gmail.com>
2015-01-31 22:16:22 -0800

    added comments

--- dirextfilter.js
+++ dirextfilter.js
@@ -1,3 +1,11 @@
+// MAKE IT MODULAR
+//  Exercise 6 of 13
+
+// This problem is the same as the previous but introduces the concept of modules. You will need to create two files to solve this.
+
+// Create a program that prints a list of files in a given directory, filtered by the extension of the files. The first argument is the directory name and the second argument is the extension filter. Print the list of files (one file per line) to the console. You must use asynchronous I/O.
+
+
 var fs = require('fs');
 var path = require('path');
 
--- httpclient_node.js
+++ httpclient_node.js
@@ -1,3 +1,8 @@
+// HTTP CLIENT
+//  Exercise 7 of 13
+
+// Write a program that performs an HTTP GET request to a URL provided to you as the first command-line argument. Write the String contents of each "data" event from the response to a new line on the console (stdout).
+
 var http = require('http');
 
 var url = process.argv[2]; // Assume first argument is always the durl
